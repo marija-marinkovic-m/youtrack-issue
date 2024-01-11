9638b17056cc2ae87d5eb68b8762888804789c76
Marija Marinkovic <marija.marinkovic@manychat.com>
2024-01-11 13:25:33 +0100

    resize handler

--- src/javascripts/components/issue-fields.js
+++ src/javascripts/components/issue-fields.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Col, Grid, Row } from '@zendeskgarden/react-grid'
+import { Alert, Title } from '@zendeskgarden/react-notifications'
 import { Button } from '@zendeskgarden/react-buttons'
 import { Field, Label, Input, Textarea } from '@zendeskgarden/react-forms'
 import { ReactComponent as PencilIcon } from '@zendeskgarden/svg-icons/src/12/pencil-stroke.svg'
@@ -8,20 +9,47 @@ import styled from 'styled-components'
 import { ButtonDots, StyledSpacer } from '../components/common'
 import useIssueForm from '../lib/useIssueForm'
 import I18n from '../lib/i18n'
+import IssueCard from './issue-card'
 
 const StyledPenIcon = styled(PencilIcon)`
   margin-right: ${(props) => props.theme.space.xs};
 `
 
-const IssueFields = ({ handleResize, textAreaRef }) => {
+const IssueFields = () => {
   const {
     subject,
     description,
     loading,
+    error,
+    success,
     setSubject,
     setDescription,
     handleSubmit
   } = useIssueForm()
+
+  const textAreaRef = React.useRef(null)
+
+  const autoResizeTextArea = () => {
+    textAreaRef.current.style.height = 'auto'
+    textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`
+  }
+
+  React.useEffect(() => {
+    autoResizeTextArea()
+  }, [])
+
+  if (error) {
+    return (
+      <Alert type='error'>
+        <Title>{I18n.t('default.error_issue_create')}</Title>
+        {I18n.t('default.error_500')}
+      </Alert>
+    )
+  }
+
+  if (success) {
+    return <IssueCard issue={success} />
+  }
 
   return (
     <Grid gutters={false}>
@@ -49,7 +77,7 @@ const IssueFields = ({ handleResize, textAreaRef }) => {
               name='description'
               onChange={(e) => {
                 setDescription(e.target.value)
-                handleResize()
+                autoResizeTextArea()
               }}
               value={description}
               disabled={loading}
--- src/javascripts/modules/issue-form.js
+++ src/javascripts/modules/issue-form.js
@@ -1,49 +1,17 @@
 import React from 'react'
 
-import { Alert, Title } from '@zendeskgarden/react-notifications'
-import IssueCard from '../components/issue-card'
 import useRelatedIssues from '../lib/useRelatedIssues'
-import useIssueForm from '../lib/useIssueForm'
 import IssueFields from '../components/issue-fields'
-import I18n from '../lib/i18n'
 
 const IssueForm = () => {
   const { issues } = useRelatedIssues()
-  const { error, success } = useIssueForm()
-
-  const textAreaRef = React.useRef(null)
-
-  const autoResizeTextArea = () => {
-    textAreaRef.current.style.height = 'auto'
-    textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`
-  }
-
-  React.useEffect(() => {
-    autoResizeTextArea()
-  }, [])
 
   if (issues.length > 0) {
     return null
   }
 
-  if (error) {
-    return (
-      <Alert type='error'>
-        <Title>{I18n.t('default.error_issue_create')}</Title>
-        {I18n.t('default.error_500')}
-      </Alert>
-    )
-  }
-
-  if (success) {
-    return <IssueCard issue={success} />
-  }
-
   return (
-    <IssueFields
-      handleResize={autoResizeTextArea}
-      textAreaRef={textAreaRef}
-    />
+    <IssueFields />
   )
 }
 
